b8bdc4e8e9ff72f8d633f276af37287305a4526f
Naz-pikachu <rishabh.jain.met19@itbhu.ac.in>
2020-06-10 00:14:32 +0530

    added navbar and fixed the transparent navbar issue

--- script.js
+++ script.js
@@ -174,3 +174,19 @@ animate = () => {
 };
 animate();
 console.log(innerWidth, innerHeight);
+
+// form here code of navbar starts
+function openNav() {
+  document.getElementById("navbar").style.width = "250px";
+  // document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("navbar").style.width = "0";
+  // document.getElementById("main").style.marginLeft = "0";
+}
+// setInterval(function () {
+// openNav();
+// }, 3000);
+//
