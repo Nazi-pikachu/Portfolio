bd9c22df7c15191fb825a2b0bc9f58d5f250158d
Naz-pikachu <rishabh.jain.met19@itbhu.ac.in>
2020-06-09 18:37:43 +0530

    commit

--- script.js
+++ script.js
@@ -1,2 +1,171 @@
-console.log("Hello");
+// console.log("Hello");
 // document.getElementById();
+$(document).ready(function () {
+  console.log("HI");
+});
+
+/************please ignore this all!!!!! main code below
+ 
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var c = canvas.getContext("2d");
+//c.fillStyle = "rgba(255,255,255,0.7)";
+//c.fillRect(canvas.width / 2 - 100 / 2, canvas.height / 2 - 100 / 2, 100, 100);
+line animation
+c.beginPath();
+c.moveTo(canvas.width / 2, 0);
+c.lineTo(0, canvas.height / 2);
+c.lineTo(canvas.width / 2, canvas.height);
+c.lineTo(canvas.width, canvas.height / 2);
+c.lineTo(canvas.width / 2, 0);
+c.lineTo(canvas.width / 2, canvas.height);
+c.strokeStyle = "rgba(255,255,255,0.5)";
+c.stroke();
+var x = Math.random() * canvas.width;
+var dx = 10;
+var y = Math.random() * canvas.height;
+var dy = 10;
+animate = () => {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.beginPath();
+    c.arc(x, y, 30, 0, 2 * Math.PI, false);
+    c.strokeStyle = "rgba(255,255,255,1)";
+    c.stroke();
+    c.fill();
+    if (x > canvas.width - 30 || x - 30 < 0)
+        dx = -dx;
+    else if (y > canvas.height - 30 || y - 30 < 0)
+        dy = -dy;
+
+    x += dx;
+    y += dy;
+}
+animate();
+
+**********************************************************/
+//let's practice some basic stuff first
+//document.getElementById('navbar').style.columnWidth = innerWidth;
+//document.getElementById('navbar').style.column = innerWidth;
+
+var canvas = document.querySelector("canvas");
+var c = canvas.getContext("2d");
+var mouse = {
+  x: undefined,
+  y: undefined,
+};
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// var color = [
+// "rgba(255, 255, 255, 0.9)",
+// "rgba(255, 255, 255, 0.9)",
+// "rgba(255, 255, 255, 0.9)",
+// "rgba(255, 255, 255, 0.9)",
+// "rgba(255, 255, 255, 0.9)",
+// ];
+
+//lets add some interactivity to our animation
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  // console.log(mouse.x, mouse.y);
+});
+/*window.addEventListener('touchmove',
+    function (event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+
+    }
+)*/
+
+//document.getElementById('navbar').style.columnWidth = innerWidth;
+window.addEventListener("resize", function (event) {
+  canvas.width = this.innerWidth;
+  canvas.height = this.innerHeight;
+  fillExtra();
+});
+
+class circle {
+  constructor(x, y, dx, dy, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.dx = dx;
+    this.dy = dy;
+    this.MinRadius = 9;
+    this.MaxRadius = 5;
+    // this.color = color[Math.floor(Math.random() * 5)];
+    this.color = "rgba(255, 255, 255, 0.9)";
+  }
+
+  //this will draw a circle on canvas
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false); // c.bezierCurveTo(innerWidth / 2, innerHeight / 2, this.x, this.y, 10, 10);
+    c.fillStyle = "	#fffafa";
+    c.fill();
+    c.strokeStyle = this.color;
+    c.stroke();
+  }
+  //this will refresh and update the canvas
+  update() {
+    //c.clearRect(0, 0, canvas.width, canvas.height);
+    //console.log(Math.sqrt(Math.pow((mouse.x - this.x), 2) + Math.pow((mouse.y - this.y), 2)));
+    // Math.abs(mouse.x - this.x);
+
+    if (this.x > canvas.width - this.r || this.x < this.r) this.dx = -this.dx;
+    if (this.y > canvas.height - this.r || this.y < this.r) this.dy = -this.dy;
+
+    // this.y += Math.sin(this.dy);
+    this.x += Math.sin(this.dx);
+
+    if (
+      mouse.x - this.x < 150 &&
+      mouse.x - this.x > -150 &&
+      mouse.y - this.y < 150 &&
+      mouse.y - this.y > -150
+    ) {
+      if (this.r < this.MaxRadius) this.r += 1;
+    } else if (this.r > this.MinRadius) this.r--;
+
+    this.draw();
+  }
+}
+
+//var cir = new circle(100, 100, 4, 4, 30); //just for debugging purpose
+//Creates an array of objects
+
+var objectArray = [];
+for (var i = 0; i < 100; i++) {
+  var r = 1;
+  var x = Math.random() * canvas.width;
+  var y = Math.random() * canvas.height;
+  var dx = Math.random() - 0.5;
+  var dy = Math.random() - 0.5;
+  objectArray.push(new circle(x, y, dx, dy, r));
+}
+//to fill extra space on resizing of the browser
+fillExtra = () => {
+  objectArray = [];
+  for (var i = 0; i < 200; i++) {
+    var r = 1 + Math.random() * 20;
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
+    var dx = (Math.random() - 0.5) * 5;
+    var dy = (Math.random() - 0.5) * 5;
+    objectArray.push(new circle(x, y, dx, dy, r));
+  }
+};
+fillExtra();
+//to create an animate loop
+animate = () => {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < objectArray.length; i++) {
+    var DizzyCircle = objectArray[i];
+    DizzyCircle.update();
+  }
+  requestAnimationFrame(animate);
+};
+animate();
+console.log(innerWidth, innerHeight);
